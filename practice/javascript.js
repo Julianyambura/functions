f1aebae0d96a0f176ce85df75166edc92bc408d5
Julia <julianyambs18@gmail.com>
2024-06-19 17:25:14 +0300

    practice

---
+++ practice/javascript.js
@@ -0,0 +1,16 @@
+// Create the student object
+const student = {
+  firstName: "Weezy",
+  lastName: "Creed",
+  age: 23,
+};
+
+if (student.age >= 78) {
+  console.log("The student is a Boomer.");
+} else if (student.age >= 45) {
+  console.log("The student is a Genz.");
+} else if (student.age >= 29) {
+  console.log("The student is a Millennial.");
+} else {
+  console.log("student is not a melleniala");
+}
